b24e3f7f914a3e7da9633b3c5fe64046e731f418
afrodriguezm1 <af.rodriguezm1@uniandes.edu.co>
2025-10-26 21:21:09 -0500

    Initial commit

---
+++ src/app/app.ts
@@ -0,0 +1,11 @@
+import { Component, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.html',
+  standalone: false,
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('ISIS2603_202520_S3_E4_Motor_Front');
+}
