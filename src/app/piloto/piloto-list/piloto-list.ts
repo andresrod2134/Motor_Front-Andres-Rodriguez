5bc15fd2479cb2b414f4c3369590a612aae13fa8
Andres <af.rodrigueza12345@uniandes.edu.co>
2025-10-28 08:51:18 -0500

    exports

---
+++ src/app/piloto/piloto-list/piloto-list.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-piloto-list',
+  standalone: false,
+  templateUrl: './piloto-list.html',
+  styleUrl: './piloto-list.css',
+})
+export class PilotoList {
+
+}
