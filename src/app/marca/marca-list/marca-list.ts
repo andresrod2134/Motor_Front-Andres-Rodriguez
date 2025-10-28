b18c9ed082e5d40cbba8670fc6aeb487924997c7
Carlos David Rodriguez Barrios <cd.rodriguezb1@uniandes.edu.co>
2025-10-28 08:40:31 -0500

    component marca-list

---
+++ src/app/marca/marca-list/marca-list.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-marca-list',
+  standalone: false,
+  templateUrl: './marca-list.html',
+  styleUrl: './marca-list.css',
+})
+export class MarcaList {
+
+}
--- src/app/marca/marca-module.ts
+++ src/app/marca/marca-module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MarcaList } from './marca-list/marca-list';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    MarcaList
+  ],
   imports: [
     CommonModule
   ]
