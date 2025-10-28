192741a9553074a49e7dd9507a6086c07a4e3054
Carlos Ramírez <cd.ramirezr1@uniandes.edu.co>
2025-10-28 08:53:19 -0500

    mecanico module

---
+++ src/app/mecanico/mecanico-list/mecanico-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mecanico-list',
+  templateUrl: './mecanico-list.component.html',
+  styleUrls: ['./mecanico-list.component.css']
+})
+export class MecanicoListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
