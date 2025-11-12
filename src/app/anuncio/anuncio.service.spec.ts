5948025adc9955326bc457fce2ac0d874ef3b57a
AndresFsantana376 <af.santana@uniandes.edu.co>
2025-11-12 09:11:10 -0500

    pestaña anuncio

---
+++ src/app/anuncio/anuncio.service.spec.ts
@@ -0,0 +1,20 @@
+import { TestBed } from '@angular/core/testing';
+
+import { BookService } from './book.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+
+describe('BookService', () => {
+  let service: BookService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [BookService]
+    });
+    service = TestBed.inject(BookService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
