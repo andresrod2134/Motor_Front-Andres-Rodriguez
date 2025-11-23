7310d44295b9c600fb643bce0efc81bf504858de
andresrod2134 <af.rodrigueza12345@uniandes.edu.co>
2025-11-22 23:56:58 -0500

    req complejidad baja

--- src/app/piloto/piloto-list/piloto-list.component.ts
+++ src/app/piloto/piloto-list/piloto-list.component.ts
@@ -32,6 +32,34 @@ export class PilotoListComponent implements OnInit {
     this.selectedPiloto = piloto;
     console.log('Piloto seleccionado:', piloto);
   }
+  ordenarPilotos(event: any): void {
+  const criterio = event.target.value;
+
+  switch (criterio) {
+
+    case "nombre":
+      this.pilotos.sort((a, b) => a.nombre.localeCompare(b.nombre));
+      break;
+
+    case "fecha":
+
+      this.pilotos.sort((a, b) => 
+        new Date(b.fechaNacimiento).getTime() - new Date(a.fechaNacimiento).getTime()
+      );
+      break;
+
+    case "pais":
+      this.pilotos.sort((a, b) => a.pais.localeCompare(b.pais));
+      break;
+
+    case "rango":
+      this.pilotos.sort((a, b) => a.rango.localeCompare(b.rango));
+      break;
+  }
+
+  this.pilotos = [...this.pilotos];
+}
+
 
   goBack(): void {
     window.history.back();
