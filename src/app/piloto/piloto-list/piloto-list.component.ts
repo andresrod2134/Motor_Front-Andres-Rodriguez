8ae1b4ec126e859db18be69e0ce993e7d6e79674
andresrod2134 <af.rodrigueza12345@uniandes.edu.co>
2025-12-04 22:05:49 -0500

    paginacion back

--- src/app/piloto/piloto-list/piloto-list.component.ts
+++ src/app/piloto/piloto-list/piloto-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { PilotoService } from './piloto.service';
 import { Piloto } from '../piloto';
+import { PageResponse } from '../page-response';
 
 @Component({
   standalone: false,
@@ -10,61 +11,73 @@ import { Piloto } from '../piloto';
 })
 export class PilotoListComponent implements OnInit {
 
-  pilotos: Array<Piloto> = [];
+  pilotos: Piloto[] = [];
+  page = 0;
+  size = 5;
+  totalPages = 0;
+
   selectedPiloto!: Piloto;
   selected = false;
 
-  constructor(private pilotoService: PilotoService) { }
+  constructor(private pilotoService: PilotoService) {}
 
-  ngOnInit() {
-    this.getPilotos();
+ngOnInit() {
+  console.log("ESTE ES EL PilotoListComponent QUE ANGULAR ESTÁ USANDO");
+  this.loadPage();
+}
+
+  loadPage() {
+    this.pilotoService.getPilotosPaged(this.page, this.size)
+      .subscribe((response: PageResponse<Piloto>) => {
+        this.pilotos = response.content;
+        this.totalPages = response.totalPages;
+      });
   }
 
-  getPilotos(): void {
-    this.pilotoService.getPilotos().subscribe((pilotos) => {
-      console.log('Pilotos que llegaron:', pilotos);
-      this.pilotos = pilotos;
-    });
+  nextPage() {
+    if (this.page < this.totalPages - 1) {
+      this.page++;
+      this.loadPage();
+    }
+  }
+
+  previousPage() {
+    if (this.page > 0) {
+      this.page--;
+     	this.loadPage();
+    }
   }
 
   onSelected(piloto: Piloto): void {
     this.selected = true;
     this.selectedPiloto = piloto;
-    console.log('Piloto seleccionado:', piloto);
   }
+
   ordenarPilotos(event: any): void {
-  const criterio = event.target.value;
+    const criterio = event.target.value;
 
-  switch (criterio) {
+    switch (criterio) {
+      case "nombre":
+        this.pilotos.sort((a, b) => a.nombre.localeCompare(b.nombre));
+        break;
+      case "fecha":
+        this.pilotos.sort((a, b) =>
+          new Date(b.fechaNacimiento).getTime() -
+          new Date(a.fechaNacimiento).getTime()
+        );
+        break;
+      case "pais":
+        this.pilotos.sort((a, b) => a.pais.localeCompare(b.pais));
+        break;
+      case "rango":
+        this.pilotos.sort((a, b) => a.rango.localeCompare(b.rango));
+        break;
+    }
 
-    case "nombre":
-      this.pilotos.sort((a, b) => a.nombre.localeCompare(b.nombre));
-      break;
-
-    case "fecha":
-
-      this.pilotos.sort((a, b) => 
-        new Date(b.fechaNacimiento).getTime() - new Date(a.fechaNacimiento).getTime()
-      );
-      break;
-
-    case "pais":
-      this.pilotos.sort((a, b) => a.pais.localeCompare(b.pais));
-      break;
-
-    case "rango":
-      this.pilotos.sort((a, b) => a.rango.localeCompare(b.rango));
-      break;
+    this.pilotos = [...this.pilotos];
   }
-
-  this.pilotos = [...this.pilotos];
-}
-
 
   goBack(): void {
     window.history.back();
   }
-
 }
-
-
--- src/app/piloto/piloto-list/piloto.service.ts
+++ src/app/piloto/piloto-list/piloto.service.ts
@@ -3,18 +3,24 @@ import { HttpClient } from '@angular/common/http';
 import { environment } from '../../../environments/environment.development';
 import { Observable } from 'rxjs';
 import { Piloto } from '../piloto';
+import { PageResponse } from '../page-response';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PilotoService {
 
-  private apiURL: string = environment.baseUrl + "pilotos"
+  private apiURL: string = environment.baseUrl + "pilotos";
 
-  getPilotos(): Observable<Piloto[]>{
-    return this.http.get<Piloto[]>(this.apiURL)
+  constructor(private http: HttpClient) {}
+
+  getPilotos(): Observable<Piloto[]> {
+    return this.http.get<Piloto[]>(this.apiURL);
   }
 
-  constructor(private http: HttpClient) { }
-
+  getPilotosPaged(page: number, size: number): Observable<PageResponse<Piloto>> {
+    return this.http.get<PageResponse<Piloto>>(
+      `${this.apiURL}/paged?page=${page}&size=${size}`
+    );
+  }
 }
